afef1f2b4bbfc65c7d64c111d43ab19045104bff
obeybook <obeybook@gmail.com>
2020-11-05 23:45:04 +0900

    react nodejs 연동 테스트

--- src/App.js
+++ src/App.js
@@ -1,78 +1,27 @@
 import React, { Component } from 'react';
-import TodoForm from './todo/todoForm.js';
-import TodoInfo from './todo/todoInfo.js';
 import './index.css';
 
 class App extends Component {
-  id = 1;
-
-  state = {
-      list: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      greeting: null
+    }
   }
 
-  listInsert = (data) => {
-    const { list } = this.state;
-
-    this.setState({
-      list: list.concat({id: this.id++, updating: false, ...data}),
-    })
-
-    this.id++;
-  }
-
-  listDelete = (id) => {
-    const { list } = this.state;
-
-    this.setState({
-      list: list.filter((item) => (
-        item.id !== id
-      )),
-    })
-  }
-
-  listUpdate = (id) => {
-    const {list} = this.state;
-
-    this.setState({
-      list: list.map((item) => (
-        item.id === id
-        ? { ...item, updating: true }
-        : { ...item }
-      )),
-    })
-  }
-
-  onSubmit = (id, data) => {
-    const {list} = this.state;
-    const { name, number } = data;
-
-    this.setState({
-      list: list.map((item) => (
-        item.id === id
-        ? { ...item, name: name, number: number, updating: false }
-        : { ...item }
-      )),
-    })
-  }
-
-  onChange = (data) => {
-    const { id, name, number } = data;
-    const {list} = this.state;
-
-    this.setState({
-      list: list.map((item) => (
-        item.id === id
-        ? { ...item, name: name, number: number }
-        : { ...item }
-      )),
-    })
+  componentDidMount() {
+    fetch('http://localhost:3002/api')
+      .then( res => res.json() )
+      .then( data => this.setState({
+          greeting: data.greeting
+        }) 
+      );
   }
 
   render() {
     return (
       <>
-        <TodoForm insert={this.listInsert}/>
-        <TodoInfo data={this.state.list} onDelete={this.listDelete} onUpdate={this.listUpdate} onChange={this.onChange} onSubmit={this.onSubmit} />
+        <div>{this.state.greeting ? <h1>{this.state.greeting}</h1>:<h1>loading...</h1>}</div>
       </>
     );
   }
